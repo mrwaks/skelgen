b435099f68d2f6b672ba955862158a36ed7c09f9
mrwaks <ouaknine.rudy@gmail.com>
2023-01-08 12:30:37 +0100

    skelgen(update): common config updated

---
+++ src/modules/config.common.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+const config = {
+  environments: [
+    'frontend', 
+    'backend',
+  ],
+  languages: [
+    'nodejs',
+  ],
+  frontendFrameworks: [
+    'vanilla',
+  ],
+  backendFrameworks: [
+    'express',
+    'vanilla',
+  ],
+};
+
+export default config;
