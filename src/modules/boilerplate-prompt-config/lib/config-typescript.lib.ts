635408ae5f4f1241a1da4fac5cfdc61359d457f4
mrwaks <ouaknine.rudy@gmail.com>
2023-01-12 09:38:31 +0100

    skelgen(code refactoring): configTypescript of prompt code refactoring

--- src/modules/boilerplate-prompt-config/lib/config-typescript.lib.ts
+++ src/modules/boilerplate-prompt-config/lib/config-typescript.lib.ts
@@ -9,8 +9,10 @@ import config from '../../config.common.js';
 // Types
 import { IBoilerPlateConfig } from '../../types.common.js';
 
-const frontendJsFrameworks = config.frontendJsFrameworks;
-const backendJsFrameworks = config.backendJsFrameworks;
+const frameworks = [
+  ...config.frontendJsFrameworks,
+  ...config.backendJsFrameworks,
+];
 
 const promptTypescript = [
   {
@@ -26,9 +28,9 @@ const configTypescript = async (boilerplateConfig: Partial<IBoilerPlateConfig>) 
   const environment = boilerplateConfig.environment;
   const framework = boilerplateConfig.framework;
 
-  if (environment === 'frontend') {
-    for (let frame of frontendJsFrameworks) {
-      if (framework === frame) {
+  if (environment === 'frontend' || environment === 'backend') {
+    for (let frame of frameworks) {
+      if (frame === framework) {
         const response = await prompt(promptTypescript);
         boilerplateConfig.typescript = response.choice === 'yes';
         break;
@@ -36,15 +38,6 @@ const configTypescript = async (boilerplateConfig: Partial<IBoilerPlateConfig>) 
     }
   }
 
-  if (environment === 'backend') {
-    for (let frame of backendJsFrameworks) {
-      if (framework === frame) {
-        const response = await prompt(promptTypescript);
-        boilerplateConfig.typescript = response.choice === 'yes';
-        break;
-      }
-    }
-  }
 };
 
 export default configTypescript;
